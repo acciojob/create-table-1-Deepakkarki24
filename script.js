6e12e7c99b7c3f683db7b708fc4543c8ceb80fed
Deepak karki <ideepakkarki@gmail.com>
2024-12-25 09:06:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ let table = document.querySelector("#sampleTable");
         let col1 = document.createElement("td");
         let col2 = document.createElement("td");
 
-        col1.innerText = `New Cell${col1Num}`;
-        col2.innerText = `New Cell${col2Num}`;
+        col1.innerText = `New Cell1`;
+        col2.innerText = `New Cell2`;
 
         row.append(col1, col2);
         table.prepend(row);
-        col1Num++;
-        col2Num++;
+        // col1Num++;
+        // col2Num++;
       }
