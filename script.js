eda01ecfc59c8716543ee1df44d474acd8ed838d
Deepak karki <ideepakkarki@gmail.com>
2024-12-25 09:11:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let table = document.querySelector("#sampleTable");
+let table = document.querySelector("tbody");
       let col1Num = 1;
       let col2Num = 2;
       function insert_Row() {
@@ -7,11 +7,12 @@ let table = document.querySelector("#sampleTable");
         let col1 = document.createElement("td");
         let col2 = document.createElement("td");
 
-        col1.innerText = `New Cell1`;
-        col2.innerText = `New Cell2`;
+        col1.innerText = `New Cell${col1Num}`;
+        col2.innerText = `New Cell${col2Num}`;
 
         row.append(col1, col2);
         table.prepend(row);
-        // col1Num++;
-        // col2Num++;
+        col1Num++;
+        // col1Num = col2Num;
+        col2Num++;
       }
