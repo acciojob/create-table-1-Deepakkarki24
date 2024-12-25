31733994a7707429780a862586cebf54933e2d33
Deepak karki <ideepakkarki@gmail.com>
2024-12-25 08:33:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function insert_Row() {
-    //Write your code here
-  
-  
-}
+let table = document.querySelector("#sampleTable");
+      function insert_Row() {
+        //Write your code here
+        let row = document.createElement("tr");
+        let col1 = document.createElement("td");
+        let col2 = document.createElement("td");
+
+        col1.innerText = "New Cell1";
+        col2.innerText = "New Cell2";
+
+        row.append(col1, col2);
+        table.prepend(row);
+      }
